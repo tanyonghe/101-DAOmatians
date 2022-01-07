c2fc1b8c34bac3c56b390a49e07e25902a64e159
unknown <tanyonghe@u.nus.edu>
2022-01-06 20:39:06 -0800

    Add graph component to proposal page

--- pages/[space]/proposal/[proposal].js
+++ pages/[space]/proposal/[proposal].js
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router'
 
+import dynamic from 'next/dynamic'
+
+const DynamicVotesChart = dynamic(() =>
+  import('../../../components/VotesChart').then((mod) => mod.VotesChart)
+)
+
+
 const Proposal = () => {
   const router = useRouter()
   const { space, proposal } = router.query
 
   return (
     <div>
+      <DynamicVotesChart />
       <p>Space: {space}</p>
       <p>Proposal: {proposal}</p>
     </div>
