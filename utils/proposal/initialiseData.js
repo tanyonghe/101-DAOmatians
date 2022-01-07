f6fc46d84ca65fe7a8ae1db1c56c8180b2e398bd
randomRadish <marcuspangyuyang@gmail.com>
2022-01-08 06:48:07 +0800

    Fix initialiseData issue with 0 uniqueChoices

--- utils/proposal/initialiseData.js
+++ utils/proposal/initialiseData.js
@@ -3,11 +3,15 @@ export const initialiseData = (uniqueChoices, votes) => {
   for (let i = 1; i <= uniqueChoices; ++i) {
     data.push({ amt: 0 });
   }
+  if (!uniqueChoices) return data;
   votes.forEach((vote) => {
-    if (Array.isArray(vote.choice))
-      vote.choice.forEach((v) => (data[v - 1].amt += vote.vp));
-    else {
-      if (typeof vote.choice === "object" && vote.choice !== null) return;
+    if (Array.isArray(vote.choice)) {
+      vote.choice.forEach((v) => {
+        console.log(v, data[v - 1]);
+        return (data[v - 1].amt += vote.vp);
+      });
+    } else {
+      if (typeof vote.choice === "object" && !vote.choice) return;
       data[vote.choice - 1].amt += vote.vp;
     }
   });
