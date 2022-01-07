4d3ba720b6283406602a98244e22ce25d1e28a9c
randomRadish <marcuspangyuyang@gmail.com>
2022-01-07 11:51:29 +0800

    Add icons and fix card for spaces

--- components/Card.js
+++ components/Card.js
@@ -11,65 +11,45 @@ import {
 const IMAGE =
   "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
 
-export default function ProductSimple(props) {
+export default function ProductSimple({ space }) {
+  let imageUrl = space.avatar;
+  if (imageUrl.startsWith("ipfs://")) {
+    const [_, id] = imageUrl.split("ipfs://");
+    imageUrl = "https://api.thegraph.com/ipfs/api/v0/cat?arg=" + id;
+  }
   return (
-    <Center py={12}>
+    <Center py={12} maxW={"xs"} w={"full"}>
       <Box
         role={"group"}
         p={6}
-        maxW={"md"}
-        w={"full"}
         bg={useColorModeValue("white", "gray.800")}
         boxShadow={"2xl"}
         rounded={"lg"}
         pos={"relative"}
+        w={"full"}
+        height={"300px"}
         zIndex={1}
       >
-        <Box
-          rounded={"lg"}
-          mt={-12}
-          pos={"relative"}
-          height={"230px"}
-          _after={{
-            transition: "all .3s ease",
-            content: '""',
-            w: "full",
-            h: "full",
-            pos: "absolute",
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${IMAGE})`,
-            filter: "blur(15px)",
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: "blur(20px)",
-            },
-          }}
-        >
+        <Box rounded={"lg"} mt={-12} pos={"relative"} height={"100px"}>
           <Image
             rounded={"lg"}
-            height={230}
-            width={282}
+            mx={"auto"}
+            height={"100px"}
+            width={"100px"}
             objectFit={"cover"}
-            src={IMAGE}
+            alt={space.symbol + " icon"}
+            src={imageUrl}
           />
         </Box>
         <Stack pt={10} align={"center"}>
           <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
-            {props.name}
+            {space.name}
           </Text>
           <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-            Nice Chair, pink
+            {space.symbol}
           </Heading>
           <Stack direction={"row"} align={"center"}>
-            <Text fontWeight={800} fontSize={"xl"}>
-              $57
-            </Text>
-            <Text textDecoration={"line-through"} color={"gray.600"}>
-              $199
-            </Text>
+            <Text color={"gray.600"}>{space.about}</Text>
           </Stack>
         </Stack>
       </Box>
